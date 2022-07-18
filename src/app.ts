ba35c6f03ecaf8f1029bf7cdcd6ae74e4f6a28d7
gshong <hgs33hgs@gmail.com>
2022-07-18 22:43:41 +0900

    + asyncLocalStorage 추가

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,34 @@
+import {AsyncLocalStorage} from 'async_hooks';
 import HyperExpress from 'hyper-express';
 const webServer = new HyperExpress.Server();
+
+const asyncLocalStorage = new AsyncLocalStorage();
+
+function logWithId(msg: string) {
+    const id = asyncLocalStorage.getStore();
+    console.log(`[${id}] ${msg}`);
+}
 
 webServer.get('/', async (req, res) => {
     res.send('Hello World');
 });
 
 webServer.post('/', async (req, res) => {
-    const reqBody = await req.json();
-    console.log(reqBody);
-    res.send('Hello World');
+    asyncLocalStorage.run(req.header('x-request-id') ?? Date.now(), async () => {
+        logWithId('a1');
+        setImmediate(async () => {
+            logWithId('a2');
+
+            const reqBody = await req.json();
+            console.log(reqBody);
+            res.send('Hello World');
+        });
+    });
 });
 
 (async () => {
     try {
-        const socket = await webServer.listen(80);
+        await webServer.listen(80);
         console.log('WebServer started on port 80');
     } catch (ex) {
         console.log('WebServer failed to start', ex);
