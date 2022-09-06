08957937ddf662d8de0eb6f936c8f62db4ede2cd
honsemiro <remocon33@naver.com>
2022-09-06 18:36:29 +0900

    + add: nxcmd 명령어 수신 라우트 추가

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,36 @@
 import {AsyncLocalStorage} from 'async_hooks';
 import HyperExpress from 'hyper-express';
+import {default as config} from '../config/local.json';
+
+const PORT: number = config.server.port;
+
 const webServer = new HyperExpress.Server();
 
 const asyncLocalStorage = new AsyncLocalStorage();
 
-function logWithId(msg: string) {
+function logWithId(msg: unknown) {
     const id = asyncLocalStorage.getStore();
-    console.log(`[${id}] ${msg}`);
+    console.log(`[${id}] ${typeof msg === 'string' ? msg : JSON.stringify(msg)}`);
 }
 
 webServer.get('/', async (req, res) => {
     res.send('Hello World');
 });
 
-webServer.post('/', async (req, res) => {
+webServer.post('/nxcommand/commands/:nxcmd', async (req, res) => {
     asyncLocalStorage.run(req.header('x-request-id') ?? Date.now(), async () => {
-        logWithId('a1');
-        setImmediate(async () => {
-            logWithId('a2');
-
-            const reqBody = await req.json();
-            console.log(reqBody);
-            res.send('Hello World');
-        });
+        const nxcmd = req.params.nxcmd;
+        logWithId(nxcmd);
+        const reqBody = await req.json();
+        logWithId(reqBody);
+        res.send('Hello World');
     });
 });
 
 (async () => {
     try {
-        await webServer.listen(80);
-        console.log('WebServer started on port 80');
+        await webServer.listen(PORT);
+        console.log('WebServer started on port', PORT);
     } catch (ex) {
         console.log('WebServer failed to start', ex);
     }
