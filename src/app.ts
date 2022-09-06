6a9757d091fcb3a182d23b9ad81162d51aa4c497
honsemiro <remocon33@naver.com>
2022-09-06 18:52:44 +0900

    + fix: 라우트 수정

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,6 @@ import {AsyncLocalStorage} from 'async_hooks';
 import HyperExpress from 'hyper-express';
 import {default as config} from '../config/local.json';
 
-const PORT: number = config.server.port;
-
-const webServer = new HyperExpress.Server();
-
 const asyncLocalStorage = new AsyncLocalStorage();
 
 function logWithId(msg: unknown) {
@@ -13,25 +9,28 @@ function logWithId(msg: unknown) {
     console.log(`[${id}] ${typeof msg === 'string' ? msg : JSON.stringify(msg)}`);
 }
 
-webServer.get('/', async (req, res) => {
-    res.send('Hello World');
-});
-
-webServer.post('/nxcommand/commands/:nxcmd', async (req, res) => {
-    asyncLocalStorage.run(req.header('x-request-id') ?? Date.now(), async () => {
-        const nxcmd = req.params.nxcmd;
-        logWithId(nxcmd);
-        const reqBody = await req.json();
-        logWithId(reqBody);
-        res.send('Hello World');
-    });
-});
-
-(async () => {
+async function main() {
+    console.log('Start server');
     try {
+        const PORT: number = config.server.port;
+        const webServer = new HyperExpress.Server();
+        webServer.get('/', async (req, res) => {
+            res.send('Hello World');
+        });
+
+        webServer.post('/nxcommand/commands/:nxcmd', async (req, res) => {
+            asyncLocalStorage.run(req.header('x-request-id') ?? Date.now(), async () => {
+                const nxcmd = req.params.nxcmd;
+                logWithId(nxcmd);
+                const reqBody = await req.json();
+                logWithId(reqBody);
+                res.send(reqBody);
+            });
+        });
         await webServer.listen(PORT);
         console.log('WebServer started on port', PORT);
     } catch (ex) {
         console.log('WebServer failed to start', ex);
     }
-})();
+}
+main();
